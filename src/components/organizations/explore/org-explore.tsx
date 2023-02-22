cf933694faf5932755e6ceda7ae18fe0dc1b3c37
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2023-02-22 15:38:52 +0100

    remove useeffect2

--- src/components/organizations/explore/org-explore.tsx
+++ src/components/organizations/explore/org-explore.tsx
@@ -1,20 +1,13 @@
 "use client";
 import { SearchBar } from "@/components/filters";
 import { TextFilter } from "@/components/filters/TextFilter";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { LocationFilter } from "@/components/filters/LocationFilter";
 import { useRouter} from "next/navigation";
-import queryString from "query-string";
 
 export const OrganizationExplore = (props: {initial: any}) => {
   const [search, setSearch] = useState(props.initial);
   const r = useRouter();
-
-  useEffect(() => {
-    console.log(search)
-    r.push('/organizations?' + queryString.stringify(search));
-  }, [search]);
-
 
   return (
     <div className="space-y-3">
